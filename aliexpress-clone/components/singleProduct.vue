8d8633c9c55a73e7a63a2f6f541aae2ee638f9d3
Emre Yiğit Kelleci <yisemu1@gmail.com>
2024-12-03 11:44:06 +0300

    Emre_5

---
+++ aliexpress-clone/components/singleProduct.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="max-w-xs p-4 border rounded-lg shadow-md group relative">
+    <div class="relative">
+      <img
+        src="https://ae-pic-a1.aliexpress-media.com/kf/S80abb93a8f8247828ec0f7d87d6f8a8eo/Square-Magic-Dice-Metal-Rotate-Cube-Fidget-Spinner-Antistress-EDC-Fingertip-Toys-Hand-Spinning-Learning-Vent.jpg_350x350xz.jpg_.webp"
+        alt="Kare sihirli zar Metal"
+        class="w-full aspect-square object-cover rounded"
+      />
+    </div>
+
+    <div class="mt-2">
+      <h3 class="text-sm font-semibold text-gray-800">
+        Kare sihirli zar Metal döndür küp Fi...
+      </h3>
+      <p class="text-xs text-gray-500">37 satıldı</p>
+      <div class="mt-1 flex items-center space-x-2">
+        <span class="text-lg font-semibold text-red-600">112,19TL</span>
+        <span class="text-sm line-through text-gray-500">260,94TL</span>
+      </div>
+      <div class="mt-2 flex items-center space-x-2">
+        <span class="px-2 py-1 text-xs text-white bg-red-600 rounded">
+          Siber Pazartesi
+        </span>
+        <span class="text-xs text-pink-600"> 148,75TL tasarruf edin... </span>
+      </div>
+      <p class="mt-1 text-xs text-gray-500">Shop1102352032 Store</p>
+    </div>
+
+    <div
+      class="mt-4 flex justify-between space-x-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+    >
+      <button
+        class="flex-1 py-2 text-sm font-medium text-white bg-gray-800 rounded shadow-md hover:bg-gray-700"
+      >
+        Ön izlemeyi görüntüle
+      </button>
+      <button
+        class="flex-1 py-2 text-sm font-medium text-white bg-gray-800 rounded shadow-md hover:bg-gray-700"
+      >
+        Benzer ürünler
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+};
+</script>
+
+<style scoped>
+</style>
